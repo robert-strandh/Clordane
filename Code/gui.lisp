81a40370ff01f91a85b1efe733198f01d629a901
Robert Strandh <robert.strandh@gmail.com>
2018-08-26 17:02:12 +0200

    Add code for displaying source and command for finding a function in an environment.

--- Code/gui.lisp
+++ Code/gui.lisp
@@ -7,7 +7,8 @@
           (backtrace :application :scroll-bars nil)
           (interaction :interactor :scroll-bars nil)
           (buttons  :application :scroll-bars nil)
-          (source :application :scroll-bars nil)
+          (source :application :scroll-bars nil
+                  :display-function 'display-source)
           (repl  :application :scroll-bars nil))
   (:layouts (default
              (clim:horizontally (:height 1000 :width 1400)
@@ -20,6 +21,13 @@
                       (8/10 (clim:scrolling (:scroll-bars t) source))
                       (2/10 (clim:scrolling (:scroll-bars t) repl))))))))
 
+(defun display-source (frame pane)
+  (let ((source (source frame)))
+    (if (null source)
+        (format pane "No source available")
+        (loop for line across source
+              do (format pane "~a~%" line)))))
+
 (defun clordane ()
   (clim:run-frame-top-level (clim:make-application-frame 'clordane)))
 
@@ -30,3 +38,18 @@
     ((environment clim:form))
   (setf (environment clim:*application-frame*)
         (eval environment)))
+
+(define-clordane-command (com-find-function :name t)
+    ((function-name clim:form))
+  (let ((environment (environment clim:*application-frame*)))
+    (if (null environment)
+        (format *standard-input* "No environment loaded")
+        (let* ((function (sicl-genv:fdefinition function-name environment))
+               (source-information
+                 (sicl-minimal-extrinsic-environment::source-information function))
+               (positions
+                 (sicl-minimal-extrinsic-environment:positions source-information)))
+          (if (null positions)
+              (format *standard-input* "No source information available")
+              (setf (source clim:*application-frame*)
+                    (sicl-source-tracking:lines (caar positions))))))))
