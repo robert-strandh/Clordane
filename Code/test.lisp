092cbd0cb1d2ea63bdb32b99eb6d9b8e4fbf4ec6
Robert Strandh <robert.strandh@gmail.com>
2024-10-18 14:11:38 +0200

    Add comment.

--- Code/test.lisp
+++ Code/test.lisp
@@ -1,5 +1,20 @@
 (cl:in-package #:common-lisp-user)
 
+;;; This is a mock-up application.  When there is a potential
+;;; breakpoint, it calls CLORDANE:POTENTIAL-BREAKPOINT with the source
+;;; information of that breakpoint.  It then waits by calling
+;;; CLORDANE:WAIT.  In a better version of Clordane and this protocol,
+;;; we imagine the use of a special variable, say CLORDANE:*FILTER*,
+;;; which contains a small-ish (maybe 1000 entries) bitvector.  The
+;;; application starts by consulting this bitvector with the address
+;;; of the instruction about to be executed, modulo the size.  If the
+;;; value is 0, there is definitely not a breakpoint there, so no need
+;;; to call POTENTIAL-BREAKPOINT or WAIT.
+
+;;; To execute the demo, make sure this file has been loaded, and then
+;;; start Clordane with (CLORDANE:CLORDANE '(BLA)).  At a breakpoint,
+;;; issue the Continue command.
+
 (defun bla ()
   (flet ((make-source (lines li1 ci1 li2 ci2)
            (cons (make-instance 'sicl-source-tracking:source-position
